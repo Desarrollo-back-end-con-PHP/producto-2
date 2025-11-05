ca9e4d7aa31a1dd749c77d3977dec778e6a2de2c
Doplax <doplax@gmail.com>
2025-11-05 19:29:23 +0100

    feat: crea la clase Model para poder extenderla a todos los modelos

---
+++ app/core/Model.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Core;
+
+use App\Core\Database;
+use \mysqli; 
+
+/**
+ * Clase Modelo Base
+ * Todos los modelos de la aplicación heredarán de esta clase.
+ * Proporciona automáticamente la conexión a la base de datos.
+ */
+class Model
+{
+
+    protected $db;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->db = $database->connection;
+    }
+}
